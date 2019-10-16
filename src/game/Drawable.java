8a819087fc843d4bb5398711d856f391b6118b36
Vladislavs Varslavans <vlad.varslavans@gmail.com>
2019-10-16 11:50:02 +0300

    Comments

--- src/game/Drawable.java
+++ src/game/Drawable.java
@@ -2,6 +2,9 @@ package game;
 
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * Something that can be drawn on a Canvas
+ */
 public interface Drawable {
 
     void draw(GraphicsContext graphicsContext);
